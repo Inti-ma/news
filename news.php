d3bc0269cd5fbb08bc19eef5dcac520be6c17775
Cam <Contillex@gmail.com>
2021-02-14 17:12:58 +0000

    Added the Valentine's Update.

--- news.php
+++ news.php
@@ -1,3 +1,15 @@
+Valentines Update|Happy Valentine's Day from everyone here at Intima!
+To celebrate, we are dropping a few new items!
+<br>
+<b>+ 4 NEW HAIRS FOR FEMALE<br>
++ 2 NEW NECKLACES FOR FEMALE<br>
++ 2 NEW GLASSES FOR FEMALE<br>
+<color=pink>Big Thanks to CandyEvilZ for these!</color>
+</b>
+<br>
+<br>
+<i>There will be a later update for male attributes, don't worry, we haven't forgotten, these elements just take longer to manufacture.</i>
+|https://cdn.discordapp.com/attachments/809912136474165298/810464098205696020/Valentine_Update_Thumb.png||||
 Welcome to Intima|First of all, the entire staff would like to welcome you all to the launch of Intima! This is a new virtual adult 3d world where you can explore, create, and live your fantasies! In light of recent events, we would like to all make a few statements. First of all, we strive to build a respectful community, where the quality of our members and the game is most important. As staff, our main goal is to support you, the community, in the most respectful way possible. Open and honest communication is not only what we will give you, but what we expect from everyone. Now go forth, bring light into the world, enjoy Intima!
 <br>
 <i>Click the image below to join our discord community!</i>|https://cdn.discordapp.com/attachments/808766552488804393/809886585100959764/placeholder_news_small.png|https://discord.com/invite/zx9ZByTdBM|||
