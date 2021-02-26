ff2e360d0478e9132b3a11bbb1499a4390c95116
Cam <Contillex@gmail.com>
2021-02-26 17:34:02 +0000

    asdad

--- newsnew.php
+++ newsnew.php
@@ -1,12 +1,12 @@
 Reset|On October 11, we are cleaning all coins and photo slots from users. On our server for a long time, coins have no value associated with various factors. The cleanup will affect everyone. The new economy will be more thoughtful, the number of coins received will be reduced, but new ways of obtaining coins, more interesting ways are planned. More details will come later|||
 Update|
-|https://board.meredian.io/uploads/monthly_2020_09/meredian-v57.png.8dd105c64756b30014f4941e60a876c1.png|||
-2D and 3D Desginer|We work look for 2D and 3D Designer. Discord SempiTerno [MDC]SempiTerno#7496|https://board.meredian.io/uploads/monthly_2020_09/1MEREDIAN-2-D-AND-3-D.jpg.ff5050f7da42f3fb87b05fd9786e4b65.jpg|http://DISCORD.GG/uQPEazf||
+|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|||
+2D and 3D Desginer|We work look for 2D and 3D Designer. Discord SempiTerno [MDC]SempiTerno#7496|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|http://DISCORD.GG/uQPEazf||
 Update|
-|https://board.meredian.io/uploads/monthly_2020_09/meredian-v57.png.8dd105c64756b30014f4941e60a876c1.png|||
+|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|||
 Gratitude|
 We receive many proposals for donations, it is valuable to us. We open such an opportunity for everyone. In addition, in gratitude you will receive thanks, colored nicknames. All details on site.
-|https://board.meredian.io/uploads/monthly_2020_08/Gr3.png.b4958df30cc115af6d0753582a434972.png|https://board.meredian.io/topic/568-donation/||||
+|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|https://board.meredian.io/topic/568-donation/||||
 Update v56|New update v56
 Man: <br>
 + Eyebrow 1 SempiTerno <br>
@@ -20,7 +20,7 @@ Woman: <br>
 - Prints 69 IvanaD, CandyEviLZ, Cherry, SeaQueen <br>
 |https://board.meredian.io/uploads/monthly_2020_06/updatejuly.jpg|||||
 Meredian anniversary|Event, CLICK for more details!
-|https://board.meredian.io/uploads/monthly_2020_05/Meredian_birthday_client.jpg|https://board.meredian.io/topic/474-meredian-anniversary-event/||||
+|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|https://board.meredian.io/topic/474-meredian-anniversary-event/||||
 Update v55|New update v55:
 + New stockings by Sempiterno
 + New hair for woman by Sempiterno
@@ -28,7 +28,7 @@ Update v55|New update v55:
 + New faceprints by Candyevilz
 + New eyebrowns for man by andruwu
 + New model of wings and added for males
-+ New prints by SeaQueen, Marta, Candyevilz and JulianoKali<br>|https://board.meredian.io/uploads/monthly_2020_05/3DXvers55.jpg||||
++ New prints by SeaQueen, Marta, Candyevilz and JulianoKali<br>|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
 Update .37|New update:
 + New hair for woman
 + 2 new hair for man
@@ -37,7 +37,7 @@ Update .37|New update:
 + New accessories
 + Rings for woman and man
 + Fixed hair shader
-+ New prints and tattoos by SeaQueen<br>|https://board.meredian.io/uploads/monthly_2020_05/3DX39037.jpg||||
++ New prints and tattoos by SeaQueen<br>|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
 Update|New update:
 + 5 new hair for woman
 + 2 new hair for man
@@ -46,4 +46,4 @@ Update|New update:
 + New face prints for woman by Bionicle
 + New face prints for man by Bionicle
 + New tattoos for woman by ManOf_Wisdom
-+ New prints by SeaQueen<br>|https://attachments.f95zone.to/2020/05/654387_Small.png||||
++ New prints by SeaQueen<br>|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
