225da9db801a7f2292ab237427fcbbb72d26bab5
Cam <Contillex@gmail.com>
2021-02-26 18:28:43 +0000

    Update newsnew.php

--- newsnew.php
+++ newsnew.php
@@ -1,1 +1,2 @@
 The Title|The Content|https://cdn.discordapp.com/attachments/808766552488804393/809886585100959764/placeholder_news_small.png||
+The Title 2|The Content 2|https://cdn.discordapp.com/attachments/808766552488804393/809886585100959764/placeholder_news_small.png||
