db470ba6e5118ee6cd53bdc89b7ab4241042234d
Cam <Contillex@gmail.com>
2021-02-26 18:17:56 +0000

    Update newsnew.php

--- newsnew.php
+++ newsnew.php
@@ -1,49 +1,37 @@
-Reset|On October 11, we are cleaning all coins and photo slots from users. On our server for a long time, coins have no value associated with various factors. The cleanup will affect everyone. The new economy will be more thoughtful, the number of coins received will be reduced, but new ways of obtaining coins, more interesting ways are planned. More details will come later|||
-Update|
-|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|||
-2D and 3D Desginer|We work look for 2D and 3D Designer. Discord SempiTerno [MDC]SempiTerno#7496|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|http://DISCORD.GG/uQPEazf||
-Update|
-|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|||
-Gratitude|
-We receive many proposals for donations, it is valuable to us. We open such an opportunity for everyone. In addition, in gratitude you will receive thanks, colored nicknames. All details on site.
-|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|https://board.meredian.io/topic/568-donation/||||
-Update v56|New update v56
-Man: <br>
-+ Eyebrow 1 SempiTerno <br>
-+ Tattoo 8 JHEKHO, Andruwu, Cherry <br>
-+ Facetatto 4 SempiTerno, CandyEviLZ, Cherry <br>
-Woman: <br>
-+ Eyebrown 1 CandyEviLZ<br>
-+ Tattoo 10 JHEKHO, SempiTerno, CandyEviLZ, Cherry, Ghug, SeaQueen <br>
-+ Facetatto 10 IvanaD, SempiTerno, CandyEviLZ, Cherry, SeaQueen <br>
-+ Stoking 18 IvanaD, CandyEviLZ <br>
-- Prints 69 IvanaD, CandyEviLZ, Cherry, SeaQueen <br>
-|https://board.meredian.io/uploads/monthly_2020_06/updatejuly.jpg|||||
-Meredian anniversary|Event, CLICK for more details!
-|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|https://board.meredian.io/topic/474-meredian-anniversary-event/||||
-Update v55|New update v55:
-+ New stockings by Sempiterno
-+ New hair for woman by Sempiterno
-+ New tattoos by Candyevilz, Seaqueen, lucifer and Marta
-+ New faceprints by Candyevilz
-+ New eyebrowns for man by andruwu
-+ New model of wings and added for males
-+ New prints by SeaQueen, Marta, Candyevilz and JulianoKali<br>|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
-Update .37|New update:
-+ New hair for woman
-+ 2 new hair for man
-+ 3 new pants for man
-+ New underwear for woman
-+ New accessories
-+ Rings for woman and man
-+ Fixed hair shader
-+ New prints and tattoos by SeaQueen<br>|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
-Update|New update:
-+ 5 new hair for woman
-+ 2 new hair for man
-+ New topwear
-+ New stockings by Bionicle
-+ New face prints for woman by Bionicle
-+ New face prints for man by Bionicle
-+ New tattoos for woman by ManOf_Wisdom
-+ New prints by SeaQueen<br>|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
+Update .37|New update:<br>
++ New hair for woman<br>
++ 2 new hair for man<br>
++ 3 new pants for man<br>
++ New underwear for woman<br>
++ New accessories<br>
++ Rings for woman and man<br>
++ Fixed hair shader<br>
++ New prints and tattoos by SeaQueen<br>|https://board.meredian.io/uploads/monthly_2020_05/3DX39037.jpg||||
+Update|New update:<br>
++ 5 new hair for woman<br>
++ 2 new hair for man<br>
++ New topwear<br>
++ New stockings by Bionicle<br>
++ New face prints for woman by Bionicle<br>
++ New face prints for man by Bionicle<br>
++ New tattoos for woman by ManOf_Wisdom<br>
++ New prints by SeaQueen<br>|https://attachments.f95zone.to/2020/05/654387_Small.png||||
+Creating Clothes|Meredian opens up the opportunity to create clothing and accessories in the game. The best designers can get reward - status on the forum and color nickname in the game. Details on the forum.|https://board.meredian.io/uploads/monthly_2020_04/1578442865_3DDesign.png.5428ad6586890d6c1b33829650b0a9f9.png|https://board.meredian.io/topic/334-creation-of-clothes-and-accessories/||||
+Update2|NEW Update. |https://board.meredian.io/uploads/monthly_2020_04/3DXupdate390.30.jpg||||
+Update|NEW Update. Piercing! Shoes Fix |https://attachments.f95zone.to/2020/02/571316_UpdSmall.png||||
+New Discord|New Discord|https://board.meredian.io/uploads/monthly_2020_02/Discord.png.41f9be32cde0efbb412239c385f6a7d6.png|https://discord.gg/uQPEazf||||
+New Forum Site|A forum has opened on the server. Now this is the main platform of community. The Discord server will be added later|https://attachments.f95zone.to/2020/01/547087_gettyimages-624878906-612x612.png|https://board.meredian.io/||||
+Recruiting|We are recruiting people who want to create prints, make-up, beards, brows and so on. If you are interested write to @Federal|https://cdn.discordapp.com/attachments/649951169095139332/658698661294899210/Recrut1.png|https://discord.gg/CnyPnaq||||
+Update 1.0.1|+ New reworked TV mod<br>
++ Money changed in real time<br>
++ Beer now cost 35 coin<br>
++ New 58 print<br>
++ New beard, brows, makeup, intimacy hair<br><br><b>Download link you can find in F95 or in Discord group</b><br><br>
++ ????? ??-???<br>
++ ????????????? ????? ? ???????? ???????<br>
++ ???? ?????? ??????? (35 ?????)<br>
++ 58 ????? ???????<br>
++ ????? ??????, ?????, ?????? ? ???????? ???????<br><br><b>?????? ?? ?????????? ????? F95 ??? ? Discord</b>
+|https://attachments.f95zone.to/2019/12/507784_ForDis.png|||
+Looking for dev on Unity3D and C#|We reached the moment when the game became fully consistent with the original. From this moment, we are ready to start developing the game ourselves. We are recruiting a team of developers on the Unity3D engine and C # language. We can add new animated poses, clothes, hair, objects for construction, and so on. If you want to the development team Write Lotus or Lucyy in Discord or in the game!|https://attachments.f95zone.to/2019/09/422601_403451_poster265s_2.png|https://discord.gg/pkjBQGz||||
+Looking for dev on Unity3D and C#|We reached the moment when the game became fully consistent with the original. From this moment, we are ready to start developing the game ourselves. We are recruiting a team of developers on the Unity3D engine and C # language. We can add new animated poses, clothes, hair, objects for construction, and so on. If you want to the development team Write Lotus or Lucyy in Discord or in the game!|https://attachments.f95zone.to/2019/09/422601_403451_poster265s_2.png|https://discord.gg/pkjBQGz||||
