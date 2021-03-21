e3ed0e249e5a2bc4eafc5ded8bf84d8aedf5f706
Cam <Contillex@gmail.com>
2021-03-21 11:46:30 +0000

    Update news.php

--- news.php
+++ news.php
@@ -1,24 +1,26 @@
-The Colorful Update|<color=#eabdff>The</color> <color=#ff9ecf>Colorful</color> <color=#9ef9ff>Update</color><br>
-<color=#ffd29e>+  Added a <b>/color</b> command, try it out in game to see what it does!</color><br>
-<color=#b8ff9e>+  2 Female Hairs</color><br>
-<color=#9ef9ff>+  4 Male Hairs</color><br>
-<color=#eabdff>+  1 Female Belt</color><br>
-<color=#ff9ecf>+  1 Male Towel</color><br>
-<color=#b8ff9e>+  1 Male Accessory</color><br>
-<br>
-<color=#ff00af><i>Big Thanks to CandyEvilZ for these!</i></color><br>
-<color=#ffd29e>+<i> Fixed a bug where when people would clap their cheeks Niki Minaj would summon. this won't happen anymore</i></color>
-|https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png||||
-Valentines Update|Happy Valentine's Day from everyone here at Intima!
-To celebrate, we are dropping a few new items!
-<br>
-+ 4 NEW HAIRS FOR FEMALE<br>
-+ 2 NEW NECKLACES FOR FEMALE<br>
-+ 2 NEW GLASSES FOR FEMALE<br>
-<color=#ff00af>Big Thanks to CandyEvilZ for these!</color>
-<br>
-<i>There will be a later update for male attributes, don't worry, we haven't forgotten, these elements just take longer to manufacture.</i>
-|https://cdn.discordapp.com/attachments/809912136474165298/810464098205696020/Valentine_Update_Thumb.png||||
-Welcome to Intima|First of all, the entire staff would like to welcome you all to the launch of Intima! This is a new virtual adult 3d world where you can explore, create, and live your fantasies! In light of recent events, we would like to all make a few statements. First of all, we strive to build a respectful community, where the quality of our members and the game is most important. As staff, our main goal is to support you, the community, in the most respectful way possible. Open and honest communication is not only what we will give you, but what we expect from everyone. Now go forth, bring light into the world, enjoy Intima!
-<br>
-<i>Click the image below to join our discord community!</i>|https://cdn.discordapp.com/attachments/808766552488804393/809886585100959764/placeholder_news_small.png|https://discord.com/invite/zx9ZByTdBM|||
+The Frisky Spring Update!|The Frisky Spring Update has arrived in Intima,
+bringing all sorts of new poses to enjoy alone and with partners!
+You can explore over 60 new poses including dances, sporty animations, idles,
+and let's not forget the wide selection of new intimate poses!
+There is something for everyone (including some new same sex options!),
+so be sure to check out the Frisky Spring Update!
+All of this will be wrapped up in a fresh new look,
+that is sure to "spring" some life into your gameplay!
+As always, you feedback is valued, so feel free to check out our
+discord server where you can post feedback and comments on the new poses and UI elements.
+Remember to have fun and be safe out there! |https://cdn.discordapp.com/attachments/809912136474165298/822946831721693244/360200.png|||| The Colorful Update|The Colorful Update
++ Added a /color command, try it out in game to see what it does!
++ 2 Female Hairs
++ 4 Male Hairs
++ 1 Female Belt
++ 1 Male Towel
++ 1 Male Accessory
+
+Big Thanks to CandyEvilZ for these!
++ Fixed a bug where when people would clap their cheeks Niki Minaj would summon. this won't happen anymore |https://cdn.discordapp.com/attachments/809912136474165298/811997889295024188/Colorful_Update_Thumb.png|||| Valentines Update|Happy Valentine's Day from everyone here at Intima! To celebrate, we are dropping a few new items!
++ 4 NEW HAIRS FOR FEMALE
++ 2 NEW NECKLACES FOR FEMALE
++ 2 NEW GLASSES FOR FEMALE
+Big Thanks to CandyEvilZ for these!
+There will be a later update for male attributes, don't worry, we haven't forgotten, these elements just take longer to manufacture. |https://cdn.discordapp.com/attachments/809912136474165298/810464098205696020/Valentine_Update_Thumb.png|||| Welcome to Intima|First of all, the entire staff would like to welcome you all to the launch of Intima! This is a new virtual adult 3d world where you can explore, create, and live your fantasies! In light of recent events, we would like to all make a few statements. First of all, we strive to build a respectful community, where the quality of our members and the game is most important. As staff, our main goal is to support you, the community, in the most respectful way possible. Open and honest communication is not only what we will give you, but what we expect from everyone. Now go forth, bring light into the world, enjoy Intima!
+Click the image below to join our discord community!|https://cdn.discordapp.com/attachments/808766552488804393/809886585100959764/placeholder_news_small.png|https://discord.com/invite/zx9ZByTdBM|||
